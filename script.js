0d1bf33ef14dd37e72f4257459dc16763733c554
carolynng <24576164+carolynng@users.noreply.github.com>
2020-05-16 09:42:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition;
-let minPosition;
-let secPosition;
+function runTheClock() {
+  var date = new Date();
+  
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
 
-var date = new Date();
+  // Convert sec/min/hr values to degrees
+  let secPosition = sec*6; // 6 is from 360/60
+  let minPosition = min*6 + sec/10 // breaks down increments between minute ticks on clock to account for seconds; 6*sec/60
+  let hrPosition = hr*30 + min/2 // 30 is from 360/12; min/2 is from min*30deg/60
 
-hrPosition = date.getHours();
-minPosition = date.getMinutes();
-secPosition = date.getSeconds();
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+// update the clock every second
+var interval = setInterval( runTheClock, 1000 );  // 1000ms = 1sec
