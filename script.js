842355b7f2b68964200be18a1d3147c01b943d0a
carolynng <24576164+carolynng@users.noreply.github.com>
2020-05-15 20:41:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+let hrPosition;
+let minPosition;
+let secPosition;
+
+var date = new Date();
+
+hrPosition = date.getHours();
+minPosition = date.getMinutes();
+secPosition = date.getSeconds();
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
