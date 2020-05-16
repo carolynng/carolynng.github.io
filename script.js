08f93632ba871f8573943b61c1f9c752bd9adef3
carolynng <24576164+carolynng@users.noreply.github.com>
2020-05-16 10:07:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+// Get the date/time value for the time at which the browser is loaded
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+// Convert sec/min/hr values to degrees
+let secPosition = sec*6; // 6 is from 360/60
+let minPosition = min*6 + sec/10 // breaks down increments between minute ticks on clock to account for seconds; 6*sec/60
+let hrPosition = hr*30 + min/2 // 30 is from 360/12; min/2 is from min*30deg/60
+
+// This function updates the positions of each hand
 function runTheClock() {
-  var date = new Date();
+  // Determine the new position of each hand given that the function is run once a second
+  hrPosition = hrPosition + (1/120)
+  minPosition = minPosition + (1/10)
+  secPosition = secPosition + 6
   
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-
-  // Convert sec/min/hr values to degrees
-  let secPosition = sec*6; // 6 is from 360/60
-  let minPosition = min*6 + sec/10 // breaks down increments between minute ticks on clock to account for seconds; 6*sec/60
-  let hrPosition = hr*30 + min/2 // 30 is from 360/12; min/2 is from min*30deg/60
-
+  // Move the hands to the new positions
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
-// update the clock every second
+// Run the function and update the clock every second
 var interval = setInterval( runTheClock, 1000 );  // 1000ms = 1sec
